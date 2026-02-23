abc235d4b3dbb015e59364ae54a9586ee20af465
faturh <fikrihabib25@gmail.com>
2026-02-23 19:07:28 +0700

    fix: remove Akbarwp references, rename project to myzyngga

--- resources/views/dashboard/layouts/footer.blade.php
+++ resources/views/dashboard/layouts/footer.blade.php
@@ -8,26 +8,14 @@
                         document.write(new Date().getFullYear() + ",");
                     </script>
                     made with <i class="ri-heart-fill text-red-500"></i> by
-                    <a href="https://github.com/Akbarwp" class="font-semibold text-slate-700 dark:text-white" target="_blank">AkbarWP</a>
+                    <a href="https://github.com/faturh" class="font-semibold text-slate-700 dark:text-white" target="_blank">faturh</a>
                     for a better web.
                 </div>
             </div>
             <div class="mt-0 w-full max-w-full shrink-0 px-3 lg:flex-none">
                 <ul class="mb-0 flex list-none flex-wrap justify-center pl-0 lg:justify-end">
                     <li class="nav-item">
-                        <a href="https://www.instagram.com/akbarwp002" class="block px-4 pb-1 pt-0 text-sm font-normal text-slate-500 transition-colors ease-in-out" target="_blank">Instagram</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="https://www.linkedin.com/in/akbar-wibowo-putra-a81810220" class="block px-4 pb-1 pt-0 text-sm font-normal text-slate-500 transition-colors ease-in-out" target="_blank">LinkedIn</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="https://portofolio-nextjs-indol.vercel.app" class="block px-4 pb-1 pt-0 text-sm font-normal text-slate-500 transition-colors ease-in-out" target="_blank">Portofolio1</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="https://akbarwp.github.io/Portofolio-TailwindCSS/" class="block px-4 pb-1 pt-0 text-sm font-normal text-slate-500 transition-colors ease-in-out" target="_blank">Portofolio2</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="https://www.showwcase.com/akbarwp" class="block px-4 pb-1 pr-0 pt-0 text-sm font-normal text-slate-500 transition-colors ease-in-out" target="_blank">Showwcase</a>
+                        <a href="https://github.com/faturh" class="block px-4 pb-1 pt-0 text-sm font-normal text-slate-500 transition-colors ease-in-out" target="_blank">GitHub</a>
                     </li>
                 </ul>
             </div>
