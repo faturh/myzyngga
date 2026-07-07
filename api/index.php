2830fa19d79189c994532b6e1560c6312e130340
Fatur <fikrihabib25@gmail.com>
2026-07-07 20:48:19 +0700

    fix: override server script names in vercel entrypoint to fix routing 404

--- api/index.php
+++ api/index.php
@@ -1,3 +1,7 @@
 <?php
+// Fix Vercel's subdirectory routing issue by resetting script names
+$_SERVER['SCRIPT_NAME'] = '/index.php';
+$_SERVER['PHP_SELF'] = '/index.php';
+
 // Forward Vercel requests to Laravel's public/index.php
 require __DIR__ . '/../public/index.php';
